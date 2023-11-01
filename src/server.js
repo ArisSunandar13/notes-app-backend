d46f9a7e60a927d031d7e48007e7f8939ef2f0d3
ArisSunandar13 <aris.s.sy13@gmail.com>
2023-11-01 20:41:24 +0700

    Add onPreResponse event

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const Hapi = require('@hapi/hapi');
 const NotesService = require('./services/postgres/NotesService');
 const notes = require('./api/notes');
 const NotesValidator = require('./validator/notes');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
@@ -24,6 +25,30 @@ const init = async () => {
       service: notesService,
       validator: NotesValidator,
     },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      if (!response.isServer) {
+        return h.continue;
+      }
+      const newResponse = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+    return h.continue;
   });
 
   await server.start();
